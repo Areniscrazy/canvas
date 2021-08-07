599f9afcc13374ac830cc6e2031835abadb0dfa5
Areniscrazy <87582667+Areniscrazy@users.noreply.github.com>
2021-08-07 11:56:07 -0700

    Add files via upload

---
+++ C81main.js
@@ -0,0 +1,160 @@
+canvas = document.getElementById("myCanvas");
+color="red";
+ctx= canvas.getContext("2d");
+ctx.beginPath();
+ctx.strokeStyle = color;
+ctx.lineWidth = 2;
+ctx.arc(200, 200, 40 , 0, 2*Math.PI);
+ctx.stroke();
+
+canvas.addEventListener("mousedown", my_mousedown);
+
+function my_mousedown(e)
+{
+color= document.getElementById("Textbox").value;
+console.log(color);
+
+mouse_x= e.clientX- canvas.offsetLeft;
+mouse_y = e.clientY- canvas.offsetTop;
+
+
+console.log("X =" + mouse_x + ",Y = " + mouse_y);
+circle(mouse_y , mouse_x);
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+function circle(mouse_y, mouse_x)
+{
+
+    ctx.beginPath();
+    ctx.strokeStyle = color;
+    ctx.lineWidth = 2;
+    ctx.arc(mouse_x,mouse_y, 40, 0, 2*Math.PI);
+    ctx.stroke();
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
